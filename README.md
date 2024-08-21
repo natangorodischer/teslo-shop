aba64449798e59e696c7d4af9aec2d424a4d2d6a
Natan Gorodischer <tu-email@example.com>
2024-08-21 15:21:28 -0400

    seed-products

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 
 1. Clonar el repositorio.
 2. Instalar dependencias: `npm install`.
-3. Crear una copia de `.env.template`, renombrarla a `.env`, y ajustar las variables de entorno según sea necesario.
+3. Crear una copia de `.env.template`, renombrarla a `.env`, y cambiar las vaiables.
 4. Levantar la base de datos con Docker: ```docker compose up -d```.
 5. Correr las migraciones de Prisma: ```npx prisma migrate dev```.
 6. Ejecutar seed ``` npm run seed ```
@@ -12,4 +12,4 @@
 
 ## Correr en producción
 
-(Puedes agregar aquí las instrucciones específicas para correr en producción si tienes alguna)
+
--- src/seed/seed-database.ts
+++ src/seed/seed-database.ts
@@ -1,18 +1,59 @@
 import { initialData } from "./seed";
+import prisma from '../lib/prisma'; 
 
 
-async function main(){
+
+async function main() {
+
+    await Promise.all ([
+        prisma.productImage.deleteMany(),
+        prisma.product.deleteMany(),
+        prisma.category.deleteMany(), 
+    ]); 
+
+    const {categories, products} = initialData
+        
+    //catergorias
+
+    const categoriesData = categories.map( (name) => ({name})); 
     
+    await prisma.category.createMany({
+        data: categoriesData
+    })
+
+    const categoriesDB = await prisma.category.findMany(); 
     
+    const categoriesMap = categoriesDB.reduce((map, category) => {
+    map [category.name.toLowerCase()]  = category.id;   
+        return map;
+    }, {} as Record<string, string>)
+    console.log(categoriesMap); 
+        
+    //productos
 
-    console.log(initialData)
+    // const {images, type,  ...product1} = products[0]
+    // await prisma.product.create({
+    //     data: {
+    //        ...product1,
+    //        categoryId: categoriesMap['shirts'] 
+    //     }
+    // })
 
-    console.log('seed ejecutado correctamente')
+
+    
+    console.log('seed ejecutado')
 }
 
-(() => {
-    main(); 
 
+
+
+
+
+(() => {
+    
+    if (process.env.NODE_ENV === 'production') return; 
+    main();
+ 
 })(); 
 
 
